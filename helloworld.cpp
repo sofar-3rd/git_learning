74ac5f080d73cdeb1daff458d76134e36a76200a
GuanlinWang <76107797+sofar-3rd@users.noreply.github.com>
2025-05-12 14:39:48 +0800

    partition

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,6 +6,20 @@ using namespace std;
 // Quick sort
 // Time complexity: O(n log n) on average, O(n^2) in the worst case
 
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
 vector<int> quicksort(vector<int>& arr, int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
